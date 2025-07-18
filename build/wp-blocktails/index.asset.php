8a1635b472c5ff4729da78f086ad700aad6a0c9d
Daniel Drave <danieldrave@gmail.com>
2025-07-18 12:30:23 +0100

    WP Blocktails - Initial scaffolding and prototyping

---
+++ build/wp-blocktails/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c8bfe83b912c4478275f');
---
+++ build/wp-blocktails/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '87920f6a23e83267fde3');
